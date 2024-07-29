2947674cba6434f97558ec2fb524174c24538557
Ronak Sharma <ronak.sharma@intimetec.com>
2024-07-29 21:48:26 +0530

    Adding the demo video of the project

---
+++ CafeteriaRecommendationSystem/Project Demo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeteriaRecommendationSystem.Project_Demo
+{
+    internal class Class1
+    {
+    }
+}
